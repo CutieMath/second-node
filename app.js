285e021fc229cfe02f8185b39fbf70a5914d2a13
CutieMath <cutie.math@protonmail.com>
2022-01-20 21:24:41 +1100

    start running my own web server

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require('express');
-express();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Baby Yummy Whoo hooo x');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+// PORT 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} x`));
