911f7a22cf4f317bf6149253ef6716468ae753c4
CutieMath <cutie.math@protonmail.com>
2022-01-20 21:36:56 +1100

    node find

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
+const coursesSavedInCode = [
+    { id: 1, name: "cutie1"},
+    { id: 2, name: "cutie2"},
+    { id: 3, name: "cutie3"},
+]
+
 app.get('/', (req, res) => {
     res.send('Baby Yummy Whoo hooo x');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(coursesSavedInCode);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    let course = coursesSavedInCode.find(c => c.id === parseInt(req.params.id));
+    if (!course){
+        res.status(404).send("The course was not found")
+    }
+    res.send(course);
 });
 
 // PORT 
