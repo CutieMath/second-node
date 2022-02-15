8ace5ff91e9537a9af64bbf74c6425e9d4d01a57
CutieMath <cutie.math@protonmail.com>
2022-02-15 20:23:50 +1100

    create custom middleware function

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const Joi = require('joi');
+const logger = require('./logger') // from local module
 const express = require('express');
 const app = express();
+
 app.use(express.json());
+app.use(logger); // use the local module
+
 
 const coursesSavedInCode = [
     { id: 1, name: "cutie1"},
