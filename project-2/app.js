6e61d653b01a755b01ec0e1d6e27caa348932dbb
CutieMath <cutie.math@protonmail.com>
2022-03-10 21:48:39 +1100

    solution1 for callback hell

--- project-2/app.js
+++ project-2/app.js
@@ -1,20 +1,35 @@
-// Asynchronous
-console.log("Before")
-getUser(1, (user) => {
-    getRepo(user.name, (repo) => {
-        getCommits(repo, (commits) => {
-            console.log(commits)
-        });
-    });
-});
-console.log("After")
+// // Asynchronous (The callback hell problem)
+// console.log("Before")
+// getUser(1, (user) => {
+//     getRepo(user.name, (repo) => {
+//         getCommits(repo, (commits) => {
+//             console.log(commits)
+//         });
+//     });
+// });
+// console.log("After")
 
-// Synchronous
-console.log("Before");
-const user = getUser(1);
-const repo = getRepo(user.name);
-const commits = getCommits(repo[0]);
-console.log("After");
+// // Synchronous
+// console.log("Before");
+// const user = getUser(1);
+// const repo = getRepo(user.name);
+// const commits = getCommits(repo[0]);
+// console.log("After");
+
+
+// Solution to the callback hell problem
+// 1- Use named function instead of anonymous
+getUser(1, getRepos);
+function getRepos(user){
+    getRepos(user.name, getCommits);
+}
+function getCommits(repos) {
+    getCommits(repos, displayCommits);
+}
+function displayCommits(commits){
+    console.log(commits);
+}
+
 
 
 // 3 ways to handle Asynchronous code
