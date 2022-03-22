3c1058b3aa7f034b030a6f3e77f9269f0f9cec93
CutieMath <cutie.math@protonmail.com>
2022-03-22 20:23:18 +1100

    create the first promise object

--- project-2/promis.js
+++ project-2/promis.js
@@ -1,1 +1,7 @@
-const p = new Promise()
+const p = new Promise((resolve, reject) => {
+    // Start async work
+    resolve(1);
+    // reject(new Error('message'));
+});
+
+p.then(result => console.log('Result:', result));
