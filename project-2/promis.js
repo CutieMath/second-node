f473aecdcbc7aac2f7f5be4af7504676f5f23598
CutieMath <cutie.math@protonmail.com>
2022-03-22 20:25:46 +1100

    Add error message for promise

--- project-2/promis.js
+++ project-2/promis.js
@@ -1,10 +1,11 @@
 const p = new Promise((resolve, reject) => {
     // Start async work
     setTimeout(() => {
-        resolve(1);
+        // resolve(1);
+        reject(new Error('message'));
     }, 2000);
-    
-    // reject(new Error('message'));
 });
 
-p.then(result => console.log('Result:', result));
+
+p.then(result => console.log('Result:', result))
+ .catch(err => console.log('Error', err.message));
