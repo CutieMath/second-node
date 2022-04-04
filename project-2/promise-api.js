fe038d66fb3f00df12899e6235d4e9d766a9cebc
CutieMath <cutie.math@protonmail.com>
2022-04-04 20:51:00 +1000

    Error and parallel operations

--- project-2/promise-api.js
+++ project-2/promise-api.js
@@ -7,7 +7,7 @@
 const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log('Operation 01...');
-        resolve("Op1 Res");
+        reject(new Error("Simulate error ~"));
     }, 2000);
 });
 
@@ -20,3 +20,4 @@ const p2 = new Promise((resolve, reject) => {
 
 Promise.all([p1, p2])
     .then(res => console.log(res))
+    .catch(err => console.log("Error: ", err.message))
