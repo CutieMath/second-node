c6c71fbdd9bb488b3363822844716200458eb83c
CutieMath <cutie.math@protonmail.com>
2022-04-04 20:43:30 +1000

    run operations in parallel :D

--- project-2/promise-api.js
+++ project-2/promise-api.js
@@ -1,5 +1,22 @@
 // const p = Promise.resolve({ id: 1 });
 // p.then(result => console.log(result));
 
-const p = Promise.reject(new Error("Error message"));
-p.catch(e => console.log(e))
+// const p = Promise.reject(new Error("Error message"));
+// p.catch(e => console.log(e))
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Operation 01...');
+        resolve("Op1 Res");
+    }, 2000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Operation 02...');
+        resolve(2);
+    }, 2000);
+});
+
+Promise.all([p1, p2])
+    .then(res => console.log(res))
